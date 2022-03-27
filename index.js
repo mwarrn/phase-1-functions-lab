d62f39d87210368a8684ca9a053a8f12c178e72c
Michael Warren <mwarrn@amazon.com>
2022-03-26 19:19:59 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const hqStreetNumber = 42;
+const block = 264;
+
+function distanceFromHqInBlocks(streetNumber) {
+    const distance = Math.abs(streetNumber - hqStreetNumber);
+    return distance;
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    const distance = distanceFromHqInBlocks(blockNumber) * block;
+    return distance;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const distance = Math.abs(startBlock - endBlock) * block;
+    return distance;
+}
+
+function calculatesFarePrice(start, destination) {
+    let price;
+    const distance = Math.abs(start - destination) * block;
+
+    if (distance < 400) {
+        price = 0;
+    }
+    else if (distance > 400 && distance < 2000) {
+        price = (distance - 400) * 0.02;
+    }
+    else if (distance > 2000 && distance < 2500) {
+        price = 25;
+    }
+    else if (distance > 2500) {
+        price = "cannot travel that far"
+    }
+    return price;
+}
